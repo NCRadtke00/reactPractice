a783d0505f76d361633f9b78c9bf114d74809e39
RadtkeN <raddnc@gmail.com>
2021-12-14 23:16:26 -0600

    styled a component within a component!

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,10 +3,13 @@ import classes from './Card.module.css'
 
 const Card = (props) => {
     return (
-        <div className={classes.card}> 
+        <div className={`${classes.card} ${props.className}`}> 
             {props.children}
         </div>
     )
 }
 
 export default Card
+
+// this is how we apply 2 styles first is intern 2nd is external
+//<div className={`${classes.card} ${props.className}`}> 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import Card from '../UI/Card'
+import classes from './AddUser.module.css';
 
-const AddUser = (props) =>{
-    const addUserHandler = (event) =>{
+const AddUser = (props) => {
+    const addUserHandler = (event) => {
         event.preventDefault();
     }
-    return(
-        <Card>
-                <from onSubmit={addUserHandler}>
-        <label htmlFor="username">UserName</label>
-        <input type="text" id="username"/>
-        <label htmlFor="age">Age</label>
-        <input type="number" id="age"/>
-        <button type="submit">Add User</button>
-    </from>
-    </Card>
+    return (
+        <Card className={classes.input}>
+            <from onSubmit={addUserHandler}>
+                <label htmlFor="username">UserName</label>
+                <input type="text" id="username" />
+                <label htmlFor="age">Age</label>
+                <input type="number" id="age" />
+                <button type="submit">Add User</button>
+            </from>
+        </Card>
     )
 };
 
