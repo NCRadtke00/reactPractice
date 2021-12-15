4cb32bea407a0fff676b9654bd66c15ce9f55681
RadtkeN <raddnc@gmail.com>
2021-12-14 23:32:40 -0600

    added button and module styling

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card'
 import classes from './AddUser.module.css';
-
+import Button from '../UI/Button'
 const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge)
+    }
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     }
     return (
         <Card className={classes.input}>
             <from onSubmit={addUserHandler}>
                 <label htmlFor="username">UserName</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" onChange={usernameChangeHandler} />
                 <label htmlFor="age">Age</label>
-                <input type="number" id="age" />
-                <button type="submit">Add User</button>
+                <input type="number" id="age" onChange={ageChangeHandler} />
+                <Button type="submit">Add User</Button>
             </from>
         </Card>
     )
