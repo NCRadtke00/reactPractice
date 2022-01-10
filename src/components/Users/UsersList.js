43e23661154366be07a024a19b41073596de8e4d
RadtkeN <raddnc@gmail.com>
2022-01-09 22:13:29 -0600

    added user id to help react/js keep track of ooo

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,18 +2,18 @@ import React from 'react'
 import Card from '../UI/Card';
 import classes from './UsersList.module.css'
 
-const UserList = (props) => {
+const UsersList = (props) => {
     return (
         <Card className={classes.users}>
             <ul>
-                {props.users.map((user) => (
-                    <li>
+                {props.users.map((user) =>
+                    <li key={user.id}>
                         {user.name}({user.age} years old)
                     </li>
-                ))}
+                )}
             </ul>
         </Card>
     )
 }
 
-export default UserList;
+export default UsersList;
