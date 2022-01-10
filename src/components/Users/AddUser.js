6e61dc61666b6c37fcde20d193063c5aa437feb5
RadtkeN <raddnc@gmail.com>
2022-01-09 23:18:53 -0600

    using a wrapper vs react.fragment or <></>

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card'
 import classes from './AddUser.module.css';
 import Button from '../UI/Button'
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper.js'
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
@@ -36,7 +37,7 @@ const AddUser = (props) => {
         setError(null);
     }
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -47,7 +48,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     )
 };
 
